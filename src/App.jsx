791c0d7786f0bdb55fa3a0fb6e276442b62ec62f
Alok Gupta <mrxguptaaa@gmail.com>
2026-07-25 18:09:14 +0530

    Deployed with changes - Footer & more

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import ProductCard from "./components/ProductCard";
+import Footer from "./components/Footer";
 
 import useProducts from "./hooks/useProducts";
 import { useMemo, useState } from "react";
@@ -83,6 +84,7 @@ function App() {
                     </div>
                 )}
             </section>
+            <Footer />
         </div>
     );
 }
